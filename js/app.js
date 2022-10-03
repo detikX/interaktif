c679371b91b063d5f76c6bb73cbbf9fee7bf875b
dediwibisono17 <dedi.wibisono17@gmail.com>
2022-10-03 11:25:58 +0700

    update awan 1

--- js/app.js
+++ js/app.js
@@ -3,8 +3,58 @@ $(document).ready(function () {
 	/* Slide variable */
 		var slideIndexBis = 1;
 		var sliding = false;
-	
+    var $isAnimatedSecond = $('.second .is-animated');
 	$('#fullpage').fullpage({
+        onLeave: function(index,nextIndex, direction){
+            // animate__animated animate__fadeIn" data-wow-delay="0.5s"
+             // first animation
+                if( index == 1 && nextIndex == 2 ) { 
+                    $isAnimatedSecond.addClass('animate__animated animate__fadeInRightBig').attr("data-wow-delay","1.5s"); 
+                    //$isAnimatedSecond.eq(0).css('animation-delay', '.3s');
+                    // $isAnimatedSecond.eq(1).css('animation-delay', '.6s');
+                    // $isAnimatedSecond.eq(2).css('animation-delay', '.9s');
+                    //$isAnimatedSecondSingle.addClass('animated animate__fadeInRightBig').css('animation-delay', '1.7s');
+                }
+
+                /**
+                 * use the following condition: 
+                 *
+                 *   else if( index == 2 && direction == 'down' ) {
+                 *
+                 * if you haven't enabled the dot navigation
+                 * or you aren't interested in the animations that occur 
+                 * when you jump (using the dot navigation) from the first section to the third one 
+                 */
+                
+                // second animation
+                else if( ( index == 1 || index == 2 ) && nextIndex == 3 ) {
+                    // $isAnimatedThird.eq(0).addClass('animated fadeInRightBig').css('animation-delay', '.3s'); 
+                    // $isAnimatedThird.eq(1).addClass('animated fadeInLeftBig').css('animation-delay', '.6s');
+                    // $isAnimatedThirdSingle.addClass('animated bounceInDown').css('animation-delay', '1.2s');
+                }
+
+                
+                /**
+                 * use the following condition:
+                 *
+                 *   else if( index == 3 && direction == 'down' ) {
+                 *
+                 * if you haven't enabled the dot navigation
+                 * or you aren't interested in the animations that occur 
+                 * when you jump (using the dot navigation) 
+                 * from the first or second section to the fourth one 
+                 */
+                
+                // third animation
+                else if( ( index == 1 || index == 2 || index == 3 ) && nextIndex == 4 ) {
+                    // $isAnimatedFourth.addClass('animated zoomIn').css('animation-delay', '.6s');
+                    // $isAnimatedFourthSingle.addClass('animated lightSpeedIn').css('animation-delay', '1.2s');
+                    // $isAnimatedFourthSingle.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+                    // $(this).removeClass('lightSpeedIn').addClass('zoomOutDown');
+                    // });
+                }
+        },
+        // first animation
 		anchors: [
 			'page1',
 			'page2',
