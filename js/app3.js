f60e3bbf6b9a2d62dc4587c4d8871294bc180180
dediwibisono17 <dedi.wibisono17@gmail.com>
2022-10-13 13:43:27 +0700

    update

--- js/app3.js
+++ js/app3.js
@@ -8,50 +8,25 @@ $(document).ready(function () {
   var $isAnimatedFive = $('.five .is-animated');
   var $isAnimatedSix = $('.six .is-animated');
 $('#fullpage').fullpage({
+      scrollOverflow: true,
+      scrollingSpeed: 700,
       onLeave: function(index,nextIndex, direction){
           // animate__animated animate__fadeIn" data-wow-delay="0.5s"
            // first animation
-              if( index == 1 && direction == "up" ) { 
-                  $isAnimatedSecond.addClass('animate__animated animate__fadeInRightBig animate__delay-2s 2s')
-                  //$isAnimatedSecond.eq(0).css('animation-delay', '.3s');
-                  // $isAnimatedSecond.eq(1).css('animation-delay', '.6s');
-                  // $isAnimatedSecond.eq(2).css('animation-delay', '.9s');
-                  //$isAnimatedSecondSingle.addClass('animated animate__fadeInRightBig').css('animation-delay', '1.7s');
-              }
-
-              else if( index == 2 && direction == "up" ) {
-                  // $isAnimatedThird.addClass('animate__animated animate__fadeInLeft animate__repeat-2	2').attr("data-wow-delay","2.5s"); 
-                  // $isAnimatedThird.eq(1).addClass('animated fadeInLeftBig').css('animation-delay', '.6s');
-                  // $isAnimatedThirdSingle.addClass('animated bounceInDown').css('animation-delay', '1.2s');
-              }
-
-         
-              // third animation
-              else if(  index == 3 && direction == "up" ) {
-                  $isAnimatedFour.eq(0).addClass('animate__animated animate__fadeInRight animate__delay-1s 1s');
-                  $isAnimatedFour.eq(1).addClass('animate__animated animate__fadeInLeft animate__delay-2s 2s');
-              }
-              else if( ( index == 1 || index == 2 || index == 3 || index == 4 ) && nextIndex == 5 ) {
-                  $isAnimatedFive.eq(0).addClass('animate__animated animate__fadeInLeft animate__delay-1s 1s');
-                  $isAnimatedFive.eq(1).addClass('animate__animated animate__fadeInLeft animate__delay-2s 2s');
-                  $isAnimatedFive.eq(2).addClass('animate__animated animate__fadeInLeft animate__delay-3s 3s');
-                  $isAnimatedFive.eq(3).addClass('animate__animated animate__fadeInRight animate__delay-4s 4s');
-                  $isAnimatedFive.eq(4).addClass('animate__animated animate__fadeInRight animate__delay-5s 5s');
-              }
-
-              else if( ( index == 1 || index == 2 || index == 3 || index == 4 || index == 5 ) && nextIndex == 6 ) {
-                  $isAnimatedSix.eq(0).addClass('animate__animated animate__fadeIn animate__delay-1s 1s');
-                  $isAnimatedSix.eq(1).addClass('animate__animated animate__fadeInLeft animate__delay-2s 2s');
-                  $isAnimatedSix.eq(2).addClass('animate__animated animate__fadeInRight animate__delay-5s 5s');
-              }
+          if (index == 1 && direction == "down") {
+            $(".second").addClass("show");
+            // $(".page__animation__city--train").velocity({right:"0"}, {delay: 500, duration: 5000});
+            // $(".page__static__build").addClass("moveDown");
+            // $(".social").fadeOut(300);
+          } 
       },
       // first animation
-  // anchors: [
-  // 	'page1',
-  // 	'page2',
-  // 	'page3',
-  // 	'page4'
-  // ],
+  anchors: [
+  	'page1',
+  	'page2',
+  	'page3',
+  	'page4'
+  ],
   sectionsColor: [
     '#00B0FF',
     '#00C853',
